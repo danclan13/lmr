afe5e91b5b3ede72f4726f1202b0768945a1287d
danclan13 <danilasniff@gmail.com>
2021-11-19 13:06:02 +0000

    Updating the code

--- lmr_core/src/main.rs
+++ lmr_core/src/main.rs
@@ -39,15 +39,14 @@ fn main() -> Result<(), Box<dyn Error>> {
         let v2 = v*(angle2.cos())+80.0;
         let v3 = -1.0*v*(angle3.cos())+80.0;
         
-        //let mut buffer_w = [251,v1 as u8,252,v2 as u8,253,v3 as u8,1];  // needs a flush
-        //i2c.block_write(0x1E, &mut buffer_w).unwrap_or_default();
+        let mut buffer_w = [251,v1 as u8,252,v2 as u8,253,v3 as u8,0xA,0x15];  // needs a flush
+        i2c.block_write(0x01, &mut buffer_w).unwrap_or_default();
 
-
-        i2c.cmd_write(251 as u8,v1 as u8).unwrap_or_default();
-        thread::sleep(Duration::from_millis(1));
-        i2c.cmd_write(252 as u8,v2 as u8).unwrap_or_default();
-        thread::sleep(Duration::from_millis(1));
-        i2c.cmd_write(253 as u8,v3 as u8).unwrap_or_default();
+        //i2c.cmd_write(251 as u8,v1 as u8).unwrap_or_default();
+        //thread::sleep(Duration::from_millis(1));
+        //i2c.cmd_write(252 as u8,v2 as u8).unwrap_or_default();
+        //thread::sleep(Duration::from_millis(1));
+        //i2c.cmd_write(253 as u8,v3 as u8).unwrap_or_default();
         println!("HLD: {}, {}, {}, V123-M: {}, {}, {}", heading, leaning_inv, direction, v1 as u8, v2 as u8, v3 as u8);}}
     }        
 }
