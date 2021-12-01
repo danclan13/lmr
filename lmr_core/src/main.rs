e3aecc24f04c6820df5cc0983fe3fedd320f66cf
danclan13 <danilasniff@gmail.com>
2021-12-01 13:34:49 +0000

    Updating the code

--- lmr_core/src/main.rs
+++ lmr_core/src/main.rs
@@ -12,7 +12,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut i2c = I2c::new()?;
     i2c.set_slave_address(0x53)?;
 
-    let mut pid = Pid::new(2.5, 0.005, 0.02, 97.0, 97.0, 97.0, 97.0, 180.0);
+    let mut pidx = Pid::new(2.5, 0.005, 0.02, 97.0, 97.0, 97.0, 97.0, 0.0);
+    let mut pidy = Pid::new(2.5, 0.005, 0.02, 97.0, 97.0, 97.0, 97.0, 0.0);
 
     loop {
         
@@ -27,26 +28,32 @@ fn main() -> Result<(), Box<dyn Error>> {
         let heading = vectstr[1].parse::<f64>().unwrap_or_default();
         let leaning = vectstr[2].parse::<f64>().unwrap_or_default();
         let direction = vectstr[3].parse::<f64>().unwrap_or_default();
-        
-        let leaning_inv = 180.0 - leaning/10.0;
-        let output = pid.next_control_output(leaning_inv);
-        
-        let v = output.output;
+
         let angle1 = PI/3.0+direction*PI/1800.0;
         let angle2 = PI/3.0-direction*PI/1800.0;
         let angle3 = direction*PI/1800.0;
+        
+        //let leaning_inv = 180.0 - leaning/10.0;
+        //let output = pid.next_control_output(leaning_inv);
+
+
+        let leaning_xpart = (leaning/10.0)*(angle3.sin());
+        let leaning_ypart = (leaning/10.0)*(angle3.cos());
+
+        let outputx = pidx.next_control_output(leaning_xpart);
+        let outputy = pidy.next_control_output(leaning_ypart);
+        
+        let vx = outputx.output;
+        let vy = outputy.output;
+        let v = (vx.powi(2)+vy.powi(2)).sqrt();
+
+ 
         let v1 = v*(angle1.cos())+80.0;
         let v2 = v*(angle2.cos())+80.0;
         let v3 = -1.0*v*(angle3.cos())+80.0;
         
-        let mut buffer_w = [251,v1 as u8,252,v2 as u8,253,v3 as u8,0xA,0xD];  // needs a flush
-        i2c.block_write(0x01, &mut buffer_w).unwrap_or_default();
-
-        //i2c.cmd_write(251 as u8,v1 as u8).unwrap_or_default();
-        //thread::sleep(Duration::from_millis(1));
-        //i2c.cmd_write(252 as u8,v2 as u8).unwrap_or_default();
-        //thread::sleep(Duration::from_millis(1));
-        //i2c.cmd_write(253 as u8,v3 as u8).unwrap_or_default();
-        println!("HLD: {}, {}, {}, V123-M: {}, {}, {}", heading, leaning_inv, direction, v1 as u8, v2 as u8, v3 as u8);}}
+        //let mut buffer_w = [251,v1 as u8,252,v2 as u8,253,v3 as u8,0xA,0xD];  // needs a flush
+        //i2c.block_write(0x01, &mut buffer_w).unwrap_or_default();
+        println!("Lx,Ly: {}, {} V123-M: {}, {}, {}", leaning_xpart, leaning_ypart, v1 as u8, v2 as u8, v3 as u8);}}
     }        
 }
